5e4667a2caa4086b226ff9b3502c649e453f9eb8
rakibul875 <rakibulislamashik78@gmail.com>
2026-06-02 22:12:25 +0600

    add user authentication and profile dropdown in Navbar

--- src/components/shear/Navbar.jsx
+++ src/components/shear/Navbar.jsx
@@ -3,12 +3,19 @@
 import React from "react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import Logo from '@/assets/image/logo.png'
+import Logo from "@/assets/image/logo.png";
 import Image from "next/image";
+import { authClient } from "@/lib/auth-client";
+import { LoginAvater } from "../LoginAvater";
+import { Button } from "@heroui/react";
 
 const Navbar = () => {
   const pathname = usePathname();
-
+  const { data: session } = authClient.useSession();
+  const user = session?.user;
+  const handelLogOut= async()=>{
+    await authClient.signOut();
+  }
   const navLinks = [
     { title: "Browse Jobs", path: "/jobs" },
     { title: "Company", path: "/company" },
@@ -17,19 +24,17 @@ const Navbar = () => {
 
   return (
     <nav className="w-full bg-transparent text-white px-4 md:px-12 py-4 flex items-center justify-between sticky top-0 z-50">
-      
       <div className="flex items-center flex-shrink-0">
         <Image
           src={Logo}
           alt="Hire Loop Logo"
-          height={40} 
-          width={160} 
+          height={40}
+          width={160}
           className="object-contain"
-          priority 
+          priority
         />
       </div>
 
-   
       <div className="hidden md:flex items-center ml-auto gap-6">
         <div className="flex items-center bg-[#1c1d20]/80 px-6 py-2.5 rounded-xl border border-gray-800/50 gap-8 text-sm font-medium">
           {navLinks.map((link, index) => {
@@ -56,12 +61,20 @@ const Navbar = () => {
         <div className="h-5 w-[1px] bg-gray-700/60 mx-1"></div>
 
         <div className="flex items-center gap-6">
-          <Link
-            href="/signin"
-            className="text-indigo-400 hover:text-indigo-300 font-medium text-sm transition-colors"
-          >
-            Sign In
-          </Link>
+          {user ? (
+            <div className="flex items-center gap-2">
+              <LoginAvater user={user}/>
+              <Button onClick={handelLogOut}>LogOut</Button>
+            </div>
+            
+          ) : (
+            <Link
+              href="/signin"
+              className="text-indigo-400 hover:text-indigo-300 font-medium text-sm transition-colors"
+            >
+              Sign In
+            </Link>
+          )}
           <button className="bg-white text-black font-semibold px-5 py-2.5 rounded-xl text-sm hover:bg-gray-200 active:scale-95 transition-all shadow-[0_4px_14px_rgba(255,255,255,0.15)]">
             Get Started
           </button>
