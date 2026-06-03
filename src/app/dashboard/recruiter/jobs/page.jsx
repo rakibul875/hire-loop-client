527032eca25cc4d27f9f51f266d68f785c40b6fa
rakibul875 <rakibulislamashik78@gmail.com>
2026-06-03 21:08:56 +0600

    add from

---
+++ src/app/dashboard/recruiter/jobs/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const recruiterJobs = () => {
+    return (
+        <div>
+            <h1>dashboard/recruiterJobs</h1>
+        </div>
+    );
+};
+
+export default recruiterJobs;
