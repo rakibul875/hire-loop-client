0a22a96e21d1ae1a95fd86bc581fab1f7c056b52
rakibul875 <rakibulislamashik78@gmail.com>
2026-06-14 11:32:13 +0600

    add protected route in application

--- src/lib/action/api/getApplication.js
+++ src/lib/action/api/getApplication.js
@@ -1,5 +1,6 @@
+import { protectFetch } from "@/lib/allPostMathode/post"
 import { handelGetSection } from "@/lib/core/allgetsection/getSection"
 
 export const getApplication=async (applicantId)=>{
-    return handelGetSection(`/application?applicantId=${applicantId}`)
+    return protectFetch(`/application?applicantId=${applicantId}`)
 }
